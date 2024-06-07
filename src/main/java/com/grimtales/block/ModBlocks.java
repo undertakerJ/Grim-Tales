bb918a6fe19dfac5e04ed4648828d854d1fb73c0
undertakerJ <justghost1242@gmail.com>
2024-06-07 21:00:57 +0300

    test loottables

--- src/main/java/com/grimtales/block/ModBlocks.java
+++ src/main/java/com/grimtales/block/ModBlocks.java
@@ -5,17 +5,20 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block CEBBITE_BLOCK = registerBlock("cebbite_block", new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK)));
-    public static final Block CEBBITE_ORE = registerBlock("cebbite_ore", new Block(FabricBlockSettings.copyOf(Blocks.DIAMOND_ORE)));
-    public static final Block DEEPSLATE_CEBBITE_ORE = registerBlock("deepslate_cebbite_ore", new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_DIAMOND_ORE)));
-    public static final Block SCULK_CEBBITE_ORE = registerBlock("sculk_cebbite_ore", new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_DIAMOND_ORE)));
+    public static final Block CEBBITE_ORE = registerBlock("cebbite_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.STONE).strength(4f)));
+    public static final Block DEEPSLATE_CEBBITE_ORE = registerBlock("deepslate_cebbite_ore", new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_DIAMOND_ORE)));
+    public static final Block SCULK_CEBBITE_ORE = registerBlock("sculk_cebbite_ore", new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_DIAMOND_ORE)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
