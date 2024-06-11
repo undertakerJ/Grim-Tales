2e1369296bb9fd0057dc42052efb867879c32657
undertakerJ <justghost1242@gmail.com>
2024-06-11 19:59:19 +0300

    fixed pickaxe look in hand, working on ore highlight(works 40%, most of the time crash)

--- src/main/java/com/grimtales/GrimTales.java
+++ src/main/java/com/grimtales/GrimTales.java
@@ -3,19 +3,47 @@ package com.grimtales;
 import com.grimtales.block.ModBlocks;
 import com.grimtales.item.ModItemGroups;
 import com.grimtales.item.ModItems;
+import com.grimtales.item.custom.CebbitePickaxeItem;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.SlimeEntity;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class GrimTales implements ModInitializer {
-	public static final String MOD_ID = "grimtales";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+import java.util.List;
 
-	@Override
-	public void onInitialize() {
-		ModItemGroups.registerItemGroups();
-		ModBlocks.registerModBlocks();
-		ModItems.registerModItems();
-	}
+import static com.grimtales.item.custom.CebbitePickaxeItem.slimes;
+
+public class GrimTales implements ModInitializer {
+    public static final String MOD_ID = "grimtales";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        List<SlimeEntity> allSlimes = CebbitePickaxeItem.slimes;
+        ServerTickEvents.END_SERVER_TICK.register((world) -> {
+            for (int i = allSlimes.size() - 1; i >= 0; i--) {
+                LivingEntity slime = allSlimes.get(i);
+                int age = slime.age;
+                if (age == 200) {
+                    slimes.remove(i);
+                    slime.remove(Entity.RemovalReason.DISCARDED);
+                }
+            }
+
+        });
+
+
+
+
+        ModItemGroups.registerItemGroups();
+        ModBlocks.registerModBlocks();
+        ModItems.registerModItems();
+    }
 }
--- src/main/java/com/grimtales/item/custom/CebbitePickaxeItem.java
+++ src/main/java/com/grimtales/item/custom/CebbitePickaxeItem.java
@@ -4,88 +4,150 @@ import com.grimtales.block.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.particle.Particle;
-import net.minecraft.client.sound.Sound;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.mob.SlimeEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.ToolMaterial;
-import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
-import net.minecraft.server.world.BlockEvent;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.text.Text;
+import net.minecraft.scoreboard.Team;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class CebbitePickaxeItem extends PickaxeItem {
+    public static final List<SlimeEntity> slimes = new ArrayList<>();
 
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
-        if(!context.getWorld().isClient()){
-            BlockPos posClicked = context.getBlockPos();
-            PlayerEntity player = context.getPlayer();
-            boolean foundBlock = false;
-            int x = posClicked.getX()+32;
-            int z = posClicked.getZ()+32;
-
-            for(int i = 0;i <= (posClicked.getY()+32); i++){
-                BlockState state = context.getWorld().getBlockState(posClicked.add(x,i,z));
-
-                if(isValuableBlock(state)){
-                    highLightValuableBlocks(posClicked.add(x,i,z), player, state.getBlock());
-                    foundBlock = true;
-                    player.sendMessage(Text.literal("Found valuable blocks"));
-                    break;
-
+        BlockPos posClicked = context.getBlockPos();
+        PlayerEntity player = context.getPlayer();
+        World world = context.getWorld();
+        int searchRadius = 8;
+        for (int x = -searchRadius; x <= searchRadius; x++) {
+            for (int y = -searchRadius; y <= searchRadius; y++) {
+                for (int z = -searchRadius; z <= searchRadius; z++) {
+                    BlockPos blockPos = posClicked.add(x, y, z);
+                    BlockState state = context.getWorld().getBlockState(blockPos);
+                    if (isValuableBlock(state)) {
+                        highLightValuableBlocks(blockPos, player, state.getBlock());
+                        if (!world.isClient) {
+                            SlimeEntity slime = new SlimeEntity(EntityType.SLIME, world);
+                            slime.setPosition(blockPos.getX() + 0.5, blockPos.getY() + 0.25, blockPos.getZ() + 0.5);
+                            slime.addStatusEffect(new StatusEffectInstance(StatusEffects.INVISIBILITY, 15000000, 0, false, false, false));
+                            slime.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 15000000, 0, false, false, false));
+                            slime.setSilent(true);
+                            slime.setAiDisabled(true);
+                            slime.setNoGravity(true);
+                            slime.setInvulnerable(true);
+                            slime.setNoGravity(true);
+                            slime.setSize(1, false);
+                            slime.disableExperienceDropping();
+                            slime.age = 0;
+                            slime.setBodyYaw(0f);
+                            slime.setHeadYaw(0f);
+                            slime.setYaw(0f);
+                            //slime.setGlowing(true);
+                            Team team = getTeamColorBlock(world, state);
+                            world.getScoreboard().addPlayerToTeam(slime.getEntityName(), team);
+                            slimes.add(slime);
+                            world.spawnEntity(slime);
+                        }
+                    }
                 }
-
             }
-
         }
-
-
-
         return ActionResult.SUCCESS;
     }
-        private void highLightValuableBlocks(BlockPos blockPos, PlayerEntity player, Block block){
-            if(player.getWorld().isClient){
-                BlockState state = player.getWorld().getBlockState(blockPos);
-                if (isValuableBlock(state)) {
-                    spawnParticleEffect(player.getWorld(), blockPos);
-                }
 
+
+    private void highLightValuableBlocks(BlockPos blockPos, PlayerEntity player, Block block) {
+        if (player.getWorld().isClient) {
+            BlockState state = player.getWorld().getBlockState(blockPos);
+            if (isValuableBlock(state)) {
+                spawnParticleEffect(player.getWorld(), blockPos);
             }
-
         }
-    private void spawnParticleEffect(World world, BlockPos blockPos) {
-        // Choose your desired particle type
-        world.playSound((PlayerEntity) world.getPlayers(),blockPos.getX(), blockPos.getY(),blockPos.getZ(),SoundEvents.BLOCK_SCULK_SHRIEKER_SHRIEK, SoundCategory.BLOCKS, 1f,1f, 100);
-        world.addParticle(ParticleTypes.SOUL_FIRE_FLAME, blockPos.getX() + 0.5, blockPos.getY() + 1.0, blockPos.getZ() + 0.5, 4, 0.1, 4);
-
     }
 
-        private boolean isValuableBlock(BlockState state){
-            return state.isOf(Blocks.IRON_ORE) ||
-                    state.isOf(Blocks.COAL_ORE) ||
-                    state.isOf(Blocks.DEEPSLATE_COAL_ORE) ||
-                    state.isOf(Blocks.DEEPSLATE_IRON_ORE) ||
-                    state.isOf(Blocks.COPPER_ORE)  ||
-                    state.isOf(Blocks.DEEPSLATE_COPPER_ORE) ||
-                    state.isOf(Blocks.GOLD_ORE) ||
-                    state.isOf(Blocks.DEEPSLATE_GOLD_ORE) ||
-                    state.isOf(Blocks.DIAMOND_ORE) ||
-                    state.isOf(Blocks.DEEPSLATE_DIAMOND_ORE) ||
-                    state.isOf(Blocks.EMERALD_ORE) ||
-                    state.isOf(Blocks.DEEPSLATE_EMERALD_ORE) ||
-                    state.isOf(ModBlocks.CEBBITE_ORE) ||
-                    state.isOf(ModBlocks.DEEPSLATE_CEBBITE_ORE) ||
-                    state.isOf(ModBlocks.SCULK_CEBBITE_ORE);
+
+    private void spawnParticleEffect(World world, BlockPos blockPos) {
+        if (world.isClient) {
+            world.addImportantParticle(ParticleTypes.SOUL_FIRE_FLAME, blockPos.getX() + 0.5, blockPos.getY() + 1.0, blockPos.getZ() + 0.5, 0, 0.05, 0);
         }
+    }
+
+    private static Team getTeamColorBlock(World world, BlockState state) {
+        Team team = null;
+        String name = null;
+        Formatting color = null;
+
+        if (state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DEEPSLATE_IRON_ORE)) {
+            name = "Iron";
+            color = Formatting.DARK_GRAY;
+        } else if (state.isOf(Blocks.COAL_ORE) || state.isOf(Blocks.DEEPSLATE_COAL_ORE)) {
+            name = "Coal";
+            color = Formatting.BLACK;
+        } else if (state.isOf(Blocks.COPPER_ORE) || state.isOf(Blocks.DEEPSLATE_COPPER_ORE)) {
+            name = "Copper";
+            color = Formatting.byColorIndex(160);
+        } else if (state.isOf(Blocks.LAPIS_ORE) || state.isOf(Blocks.DEEPSLATE_LAPIS_ORE)) {
+            name = "Lapis";
+            color = Formatting.DARK_BLUE;
+        } else if (state.isOf(Blocks.GOLD_ORE) || state.isOf(Blocks.DEEPSLATE_GOLD_ORE)) {
+            name = "Gold";
+            color = Formatting.YELLOW;
+        } else if (state.isOf(Blocks.DIAMOND_ORE) || state.isOf(Blocks.DEEPSLATE_DIAMOND_ORE)) {
+            name = "Diamond";
+            color = Formatting.AQUA;
+        } else if (state.isOf(Blocks.EMERALD_ORE) || state.isOf(Blocks.DEEPSLATE_EMERALD_ORE)) {
+            name = "Emerald";
+            color = Formatting.DARK_GREEN;
+        } else if (state.isOf(ModBlocks.CEBBITE_ORE) ||
+                state.isOf(ModBlocks.DEEPSLATE_CEBBITE_ORE) ||
+                state.isOf(ModBlocks.SCULK_CEBBITE_ORE)) {
+            name = "Cebbite";
+            color = Formatting.DARK_PURPLE;
+        }
+
+        team = world.getScoreboard().getPlayerTeam(name);
+        if (team == null && name != null) {
+            team = world.getScoreboard().addTeam(name);
+            team.setColor(color);
+        }
+
+        return team;
+    }
+
+    private boolean isValuableBlock(BlockState state) {
+        return state.isOf(Blocks.IRON_ORE) ||
+                state.isOf(Blocks.COAL_ORE) ||
+                state.isOf(Blocks.DEEPSLATE_COAL_ORE) ||
+                state.isOf(Blocks.DEEPSLATE_IRON_ORE) ||
+                state.isOf(Blocks.COPPER_ORE) ||
+                state.isOf(Blocks.DEEPSLATE_COPPER_ORE) ||
+                state.isOf(Blocks.GOLD_ORE) ||
+                state.isOf(Blocks.DEEPSLATE_GOLD_ORE) ||
+                state.isOf(Blocks.DIAMOND_ORE) ||
+                state.isOf(Blocks.DEEPSLATE_DIAMOND_ORE) ||
+                state.isOf(Blocks.EMERALD_ORE) ||
+                state.isOf(Blocks.DEEPSLATE_EMERALD_ORE) ||
+                state.isOf(Blocks.LAPIS_ORE) ||
+                state.isOf(Blocks.DEEPSLATE_LAPIS_ORE) ||
+                state.isOf(Blocks.REDSTONE_ORE) ||
+                state.isOf(Blocks.DEEPSLATE_REDSTONE_ORE) ||
+                state.isOf(ModBlocks.CEBBITE_ORE) ||
+                state.isOf(ModBlocks.DEEPSLATE_CEBBITE_ORE) ||
+                state.isOf(ModBlocks.SCULK_CEBBITE_ORE);
+    }
 
     public CebbitePickaxeItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
         super(material, attackDamage, attackSpeed, settings);
